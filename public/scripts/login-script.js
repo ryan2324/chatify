0cb19cbece5d1fc4dfb51a867211db97d6c2082c
unknown <jayryan17380@gmail.com>
2022-05-01 01:05:54 +0800

    error login

--- public/scripts/login-script.js
+++ public/scripts/login-script.js
@@ -4,13 +4,18 @@ const loginBtn = $('#loginBtn');
 
 const login = async () =>{
     localStorage.removeItem('chatify')
-    const response = await axios.post('/user/login',{
-        username: inputUsername.val(),
-        password: inputPassword.val()
-    })
-    if(response.status == 200){
-        localStorage.setItem('chatify', JSON.stringify(response.data))
-        window.location.href = '/'
+    try{
+        const response = await axios.post('/user/login',{
+            username: inputUsername.val(),
+            password: inputPassword.val()
+        })
+        if(response.status == 200){
+            localStorage.setItem('chatify', JSON.stringify(response.data))
+            window.location.href = '/'
+        }
+        
+    }catch(error){
+        console.log(error)
     }
     
 }
