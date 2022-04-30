6e9ddc6d9d74cdbb645fc7951e1d312ac33808ec
unknown <jayryan17380@gmail.com>
2022-05-01 01:38:35 +0800

    s

--- public/scripts/login-script.js
+++ public/scripts/login-script.js
@@ -3,22 +3,20 @@ const inputPassword = $('#input-password');
 const loginBtn = $('#loginBtn');
 
 const login = async () =>{
-    // localStorage.removeItem('chatify')
-    // try{
-    //     const response = await axios.post('/user/login',{
-    //         username: inputUsername.val(),
-    //         password: inputPassword.val()
-    //     })
-    //     if(response.status == 200){
-    //         localStorage.setItem('chatify', JSON.stringify(response.data))
-    //         window.location.href = '/'
-    //     }
+    localStorage.removeItem('chatify')
+    try{
+        const response = await axios.post('/user/login',{
+            username: inputUsername.val(),
+            password: inputPassword.val()
+        })
+        if(response.status == 200){
+            localStorage.setItem('chatify', JSON.stringify(response.data))
+            window.location.href = '/'
+        }
         
-    // }catch(error){
-    //     console.log(error)
-    // }
-    const response = await axios.post('/user/login')
-    console.log(response)
+    }catch(error){
+        console.log(error)
+    }
     
 }
 
